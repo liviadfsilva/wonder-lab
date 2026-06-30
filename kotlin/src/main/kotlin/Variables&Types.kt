c5d0f74ad50b19f4ac864ce4868cc53181732351
liviadfsilva <liviadfsilva@gmail.com>
2026-06-30 17:00:45 -0300

    feat: create variables and types file

---
+++ kotlin/src/main/kotlin/Variables&Types.kt
@@ -0,0 +1,21 @@
+fun main(){
+
+    // stands for value, it's a variable
+    val x = 13
+
+    // integer
+    val y: Int = 31
+
+    // float
+    val z = 131f
+
+    // boolean
+    val a = true
+
+    // string
+    val name = "Lily"
+
+    // stands for variable, also a variable but its value can change.
+    var age = 24
+    age = 25
+}
